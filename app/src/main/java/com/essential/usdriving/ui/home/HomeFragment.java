468c42b43a842e4d2419a4d3fc64a727c1d9cb48
Thanh Nguyen Huy <thanhnh.411@gmail.com>
2016-04-21 23:29:02 +0700

    - fix stuff

--- app/src/main/java/com/essential/usdriving/ui/home/HomeFragment.java
+++ app/src/main/java/com/essential/usdriving/ui/home/HomeFragment.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.essential.usdriving.BuildConfig;
 import com.essential.usdriving.R;
 import com.essential.usdriving.app.BaseFragment;
 import com.essential.usdriving.app.DMVActivity;
@@ -221,7 +222,11 @@ public class HomeFragment extends BaseFragment {
 
         mFabMoreApps.setOnClickListener(floatingMenuItemClickListener);
         mFabRateUs.setOnClickListener(floatingMenuItemClickListener);
-        mFabRemoveAds.setOnClickListener(floatingMenuItemClickListener);
+        if(!BuildConfig.IS_PRO_VERSION) {
+            mFabRemoveAds.setOnClickListener(floatingMenuItemClickListener);
+        }else{
+            mFabRemoveAds.setVisibility(View.GONE);
+        }
     }
 
     private void loadState() {
